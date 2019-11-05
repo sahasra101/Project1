e3bf05b4f7b2b1ec80733c81da1bd21a10471166
sahasra101 <ajaykiri7@gmail.com>
2019-11-05 11:41:47 -0500

    functionality to google maps: only one marker at a time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 // YELP BELOW
 
 function yelp(city, category = "Dining", yelpDisplay = "#yelp-display-1") {
-  
+
     // var categors = ["Outdoor Dining", "Museums", "Nature"];
 
     // for (i = 0; i < categors.length; i++) {}
@@ -22,7 +22,7 @@ function yelp(city, category = "Dining", yelpDisplay = "#yelp-display-1") {
 
             console.log("Ajax Response below");
             console.log(response);
-            
+
             $(yelpDisplay).empty();
 
             // var results = response.data;
@@ -40,7 +40,7 @@ function yelp(city, category = "Dining", yelpDisplay = "#yelp-display-1") {
                 var bLocation = response.businesses[j].location.display_address;
                 var lat = response.businesses[j].coordinates.latitude;
                 var lng = response.businesses[j].coordinates.longitude;
-                var myLatlng = {lat, lng};
+                var myLatlng = { lat, lng };
                 var addBtn = $("<button>").addClass("btn btn-info add-to-map-btn").text("Map It");
                 addBtn.attr("data-lng", lng);
                 addBtn.attr("data-lat", lat);
@@ -50,8 +50,8 @@ function yelp(city, category = "Dining", yelpDisplay = "#yelp-display-1") {
                 // console.log("Phone = " + bPhone);
                 // console.log("Address = " + bLocation);
                 // console.log("Rating = " + bRating);
-                 console.log("Latitude, longitute = " + lat, lng);
-                 console.log(myLatlng);
+                console.log("Latitude, longitute = " + lat, lng);
+                console.log(myLatlng);
 
                 var imageRender = $("<img>")
                 imageRender.attr("src", bImage);
@@ -66,15 +66,13 @@ function yelp(city, category = "Dining", yelpDisplay = "#yelp-display-1") {
                 $(yelpDisplay).append(yelpResult);
                 console.log(yelpResult)
             }
-     
-      })
+
+        })
 }
-    
-        
 
 // On page load, 
-    // display top 5 search results for 
-        // 3 pre-selected categories for 1 pre-selected city 
+// display top 5 search results for 
+// 3 pre-selected categories for 1 pre-selected city 
 $(document).ready(function () {
 
     console.log("Page load function works");
@@ -94,7 +92,7 @@ $(document).ready(function () {
     yelp(cityName, category2, "#yelp-display-2");
     yelp(cityName, category3, "#yelp-display-3")
 
-    $("#city-name").on("keyup", function(event) {
+    $("#city-name").on("keyup", function (event) {
         event.preventDefault();
         cityName = $(this).val();
 
@@ -102,31 +100,27 @@ $(document).ready(function () {
         yelp(cityName, category2, "#yelp-display-2");
         yelp(cityName, category3, "#yelp-display-3");
         console.log(cityName);
-        
     })
 
-    $("#yelp-search-1").on("keyup", function(event) {
+    $("#yelp-search-1").on("keyup", function (event) {
         event.preventDefault();
         category1 = $("#yelp-search-1").val();
 
         yelp(cityName, category1, "#yelp-display-1");
         console.log(category1);
-       // console.log("city1 " + cityName)
+        // console.log("city1 " + cityName)
     })
 
-    $("#yelp-search-2").on("keyup", function(event) {
+    $("#yelp-search-2").on("keyup", function (event) {
         event.preventDefault();
         category2 = $("#yelp-search-2").val()
 
         yelp(cityName, category2, "#yelp-display-2");
         console.log(category2)
         //console.log("city2 " + cityName)
-        
-        
-
     })
 
-    $("#yelp-search-3").on("keyup", function(event) {
+    $("#yelp-search-3").on("keyup", function (event) {
         event.preventDefault();
         category3 = $("#yelp-search-3").val()
 
@@ -134,90 +128,13 @@ $(document).ready(function () {
         console.log(category3)
         //console.log("city3 " + cityName)
     })
-
-
 });
-
-// On click of search button,
-    // empty the divs under yelp-display-1 (or 2 or 3)
-    // and display top 5 search results for 
-        // each user input category for user input city 
-// $("#search-btn-1").on("click", function (event) {
-
-//     event.preventDefault();
-
-//     var city = $("#city-name").val().trim();
-//     var category = $("#yelp-search-1").val().trim();
-//     console.log("City = " + city + ", Search = " + category);
-
-//     $("#yelp-display-1").empty(); // empty yelp-display div
-
-
-//     var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" + category + "&location=" + city
-
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET",
-//         headers: {
-//             Authorization: "Bearer f_OWbSivdwPfDyl30viNm1pnNTDjKRkxJILe56UtQARE0RCvNNOoDY11kZPHD3XBxpDDsHLcQ4gUlUfE_-CxM89He8J5bEgGByTO56_pk-LILybh25BE7NgrRLQsXXYx",
-//         },
-//         dataType: 'json',
-
-//     })
-//         .then(function (response) {
-
-//             console.log("Ajax Response from user input below");
-//             console.log(response);
-//             // var testDiv = $("#test");
-//             // var results = response.data
-
-//             for (j = 0; j < 5; j++) {
-
-                var bName = response.businesses[j].name;
-                var bImage = response.businesses[j].image_url;
-                var bRating = response.businesses[j].rating;
-                var bPhone = response.businesses[j].phone;
-                var bLocation = response.businesses[j].location.display_address;
-                var lat = response.businesses[j].coordinates.latitude;
-                var lng = response.businesses[j].coordinates.longitude;
-
-                console.log("Business Name = " + bName);
-                console.log("Image URL = " + bImage);
-                console.log("Phone = " + bPhone);
-                console.log("Address = " + bLocation);
-                console.log("Rating = " + bRating);
-                console.log("Latitude, longitute = " + lat, lng);
-
-//                 console.log("Business Name = " + bName);
-//                 console.log("Image URL = " + bImage);
-//                 console.log("Phone = " + bPhone);
-//                 console.log("Address = " + bLocation);
-//                 console.log("Rating = " + bRating);
-//                 console.log("Latitude, longitute = " + lat, lon);
-
-//                 var imageRender = $("<img>")
-//                 imageRender.attr("src", bImage);
-//                 imageRender.attr("class", "responsive-img", bImage);
-//                 yelpResult.append(imageRender);
-//                 yelpResult.append("<b>" + bName + "</b>");
-//                 yelpResult.append("</br><i class=\"fas fa-star\"></i> " + bRating + " Stars");
-//                 yelpResult.append("</br><i class=\"fas fa-map-marker-alt\"></i> " + bLocation);
-//                 yelpResult.append("</br><i class=\"fas fa-phone-alt\"></i> " + bPhone);
-
-//                 $("#yelp-display-1").append(yelpResult);
-                
-
-
-
-               
-//             };
-//         })
-// })
 
 // GOOGLE MAPS BELOW
 
 var map;
 var geocoder;
+var markers = [];
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -231,15 +148,22 @@ function initMap() {
     });
 }
 
-$(document).on("click", ".add-to-map-btn", function() {
+$(document).on("click", ".add-to-map-btn", function () {
+    deleteMarkers();
     var lat = parseFloat($(this).attr("data-lat"));
     var lng = parseFloat($(this).attr("data-lng"));
     map.setCenter({ lat, lng });
+    console.log({ lat, lng });
     var marker = new google.maps.Marker({
         map: map,
         position: { lat, lng }
     });
-})
+    markers.push(marker);
+
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#map").offset().top
+    }, 1000);
+});
 
 function geocodeAddress(geocoder, resultsMap) {
     var address = document.getElementById('address').value;
@@ -254,10 +178,23 @@ function geocodeAddress(geocoder, resultsMap) {
         } else {
             alert('Geocode was not successful for the following reason: ' + status);
         }
+        markers.push(marker);
     });
 }
 
-$(document).on("click", "#clearMarkers", function (resultsMap) {
-    marker.setMap(null);
-});
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
 
+function clearMarkers() {
+    setMapOnAll(null);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+}
